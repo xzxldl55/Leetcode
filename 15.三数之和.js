e068ece01592eaa157c50f93e14d8c45f3e7bf3a
xzxldl55 <476630095@qq.com>
2022-07-01 09:21:04 +0800

    feat: 15

---
+++ 15.三数之和.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=javascript
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const len = nums.length;
+    const ret = [];
+    if (len < 3) {
+        return ret;
+    }
+    if (len === 3) {
+        return !nums.reduce((p, v) => p + v) ? ret : [nums];
+    }
+
+    // len > 4的情况
+    
+};
+// @lc code=end
+
