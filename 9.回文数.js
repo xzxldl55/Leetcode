475b34b2a79a6052230c7da3c931665386e74dd2
xzxldl55 <476630095@qq.com>
2022-07-15 18:44:49 +0800

    feat: 9. 回文数

--- 9.回文数.js
+++ 9.回文数.js
@@ -9,35 +9,25 @@
  * @param {number} x
  * @return {boolean}
  */
-// var isPalindrome = function(x) {
-//     if (x < 0)
-//         return false;
-//     if (x === 0 || x < 10)
-//         return true;
-
-//     const str = x.toString();
-//     const isOdd = str.length % 2 === 0;
-//     const midI = Math.floor(str.length / 2);
-//     const leftStr = str.slice(0, midI);
-//     const rightStr = str.slice(isOdd ? midI : midI + 1);
-
-//     if (leftStr === rightStr.split('').reverse().join('')) {
-//         return true;
-//     }
-//     return false;
-// };
-
-// 纯数字解法
-var isPalindrome = (x) => {
-    if (x < 0 || x % 10 === 0) return false;
-    if (x === 0 || x < 10) return true;
-
-    let div = 1;
-
-    // 获取当前
-    while (x / div >= 10) div *= 10;
-    while (div >= 10) {
-        x / div;
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    } else if (x % 10 === 0 && x !== 0) {
+        return false
     }
+
+    let revertHalfNum = 0
+
+    // 当取值一半 大于/等于 原数 时说明我们已经取到了一半/以上
+    while (x > revertHalfNum) {
+
+        // 不断从后取值 --> 直到取到了原数的一半为止
+        revertHalfNum = revertHalfNum * 10 + x % 10
+        x = parseInt(x / 10)
+    }
+
+    return revertHalfNum === x || parseInt(revertHalfNum / 10) === x
 };
 // @lc code=end
+
+console.log(isPalindrome(123321))
