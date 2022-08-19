dab8eb50352424388c9a6a997c5bea9e230203ca
xzxldl55 <22699218+xzxldl55@users.noreply.github.com>
2022-08-19 20:18:17 +0800

    feat: 15.三数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -21,5 +21,41 @@ var twoSum = function (nums, target) {
         }
     }
 };
+
+var twoSumPointer = function (nums, target) {
+    const len = nums.length;
+    if (len === 2 && nums[0] + nums[1] === target) {
+        return [0, 1];
+    }
+
+    // 记录原始位置
+    const map = {}
+    nums.forEach((n, i) => {
+        if (map[n]) {
+            map[n].push(i)
+        } else {
+            map[n] = [i]
+        }
+    })
+
+    // 排序
+    nums = nums.sort((a, b) => a - b);
+    console.log(nums)
+    let left = 0,
+        right = len - 1;
+
+    // 双指针遍历，得到解后，查询值的原下标
+    while (left < right) {
+        const result = nums[left] + nums[right];
+
+        if (result === target) {
+            return [map[nums[left]].pop(), map[nums[right]]].pop();
+        } else if (result < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+}
 // @lc code=end
-twoSum([2, 7, 11, 15], 9);
+console.log(twoSumPointer([0, 2, 6, 3, 3], 6))
--- 15.三数之和.js
+++ 15.三数之和.js
@@ -2,6 +2,10 @@
  * @lc app=leetcode.cn id=15 lang=javascript
  *
  * [15] 三数之和
+ *
+ * 先固定一个值，将其变成两数之和
+ *
+ * 为了去重，则需要将数组先排序，在通过前后值是否相同比对去重
  */
 
 // @lc code=start
@@ -16,9 +20,55 @@ var threeSum = function (nums) {
         return ret;
     }
     if (len === 3) {
-        return !nums.reduce((p, v) => p + v) ? ret : [nums];
+        return !nums.reduce((p, v) => p + v, 0) ? [nums] : [];
     }
 
     // len > 4的情况
+    nums = nums.sort((a, b) => a - b)
+    for (let i = 0; i < len; i++) {
+        const target = nums[i];
+
+        // target为最左侧最小值（target + left + right），如果大于0，说明其组合不可能小于0,
+        // 右侧都是 > 0，可以结束循环
+        if (target > 0) {
+            break;
+        }
+
+        // nums[i] === nums[i - 1] 表示当前i得到的数据会跟上一次迭代重复，可跳过
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1,
+            right = len - 1;
+
+        // 左右指针不能重叠
+        while (left < right) {
+            const result = target + nums[left] + nums[right];
+            if (result === 0) {
+
+                // 记录本次结果
+                ret.push([target, nums[left], nums[right]]);
+
+                // 【驱虫】下次去值等于本次，则跳过下次，知道找到不同值位置
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+                left++;
+                right--;
+            } else if (result < 0) { // 太小了，left右移，增大result
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return ret;
 };
 // @lc code=end
+
+console.log(threeSum([-2,0,1,1,2]))
